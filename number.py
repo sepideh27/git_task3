aecb51c32548ea86ebf44bdac3444c7427fbb623
SepidehStray <Sepideh@spatialedge.co.za>
2025-02-26 09:07:34 +0200

    convert the input to int

---
+++ number.py
@@ -0,0 +1,17 @@
+
+# Asking the user to enter three number
+first_num = int(input("Please enter a number: "))
+second_num = int(input("Please enter another number: "))
+third_num = int(input("Please enter the last number: "))
+
+sum_all = first_num + second_num + third_num  # Calculates the sum of all three numbers
+print(sum_all)
+
+sub_num = first_num - second_num  # Calculates the difference between first_num and second_num
+print(sub_num)
+
+multi_num = third_num * first_num  # Multiplies third_num by first_num
+print(multi_num)
+  
+
+print (sum_all / third_num)  # Divides sum_all by third_num
